124c4378beee443c6f66ffc6793450a14b44a4ec
Islam Tahiraj <islam.tahiraj@hotmail.com>
2026-01-27 21:40:31 +0100

    feat: add tooltip and dropdown menu component for work order details

---
+++ src/app/shared/components/nao-dropdown-menu/nao-dropdown-menu.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NaoDropdownMenuComponent } from './nao-dropdown-menu.component';
+
+describe('NaoDropdownMenuComponent', () => {
+  let component: NaoDropdownMenuComponent;
+  let fixture: ComponentFixture<NaoDropdownMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NaoDropdownMenuComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NaoDropdownMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/shared/components/nao-tooltip/nao-tooltip.component.ts
+++ src/app/shared/components/nao-tooltip/nao-tooltip.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nao-tooltip',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './nao-tooltip.component.html',
   styleUrl: './nao-tooltip.component.scss'
 })
 export class NaoTooltipComponent {
-
+  @Input() text = '';
+  @Input() position: 'top' | 'bottom' | 'left' | 'right' = 'top';
 }
